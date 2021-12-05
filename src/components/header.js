2a31617dc7e2e41c390fcae39c74508f103e38b7
Tracey <traceylea9@gmail.com>
2021-12-05 23:06:19 +1100

    header / nav bar implemented but issues getting appear on scroll to work

---
+++ src/components/header.js
@@ -0,0 +1,154 @@
+import React from "react";
+import { Box } from "@mui/system";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  MenuItem,
+  Menu,
+} from "@mui/material";
+import { alpha, styled } from "@mui/system";
+
+const TextButton = styled(Button)(({ theme }) => ({
+  textTransform: "none",
+  borderRadius: "50px",
+  color: alpha("#000000", 0.54),
+  padding: 16,
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.primary.light, 0.14),
+  },
+}));
+
+const NavButton = styled(Button)(({ theme }) => ({
+  borderRadius: "50px",
+  paddingTop: 16,
+  paddingBottom: 16,
+  color: alpha("#000000", 0.54),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.primary.light, 0.14),
+  },
+}));
+
+const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        position: "sticky",
+        top: "0",
+        zIndex: "100",
+        width: "100%",
+      }}
+      className="header"
+    >
+      <img
+        style={{
+          objectFit: "contain",
+          height: "100px",
+          paddingTop: "20px",
+          paddingLeft: "14px",
+          paddingRight: "14px",
+        }}
+        className="header_icon"
+        src="./img/ssLogo.png"
+        alt="swarm saviour logo with bee"
+      />
+      <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+        <OutlinedInput
+          sx={{ borderRadius: "50px" }}
+          id="outlined-adornment-password"
+          type="text"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton aria-label="Search" edge="end">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+      {/* <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flex: "1",
+          maxWidth: "fit-content",
+          padding: "10px",
+          height: "30px",
+          border: "1px solid lightgray",
+          borderRadius: "999px",
+        }}
+        className="header_centre"
+      >
+        <TextField sx={{ border: "none", width: "250px" }} type="text" />
+        
+      </Box> */}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "20",
+          marginRight: "80px",
+        }}
+        className="header_right"
+      >
+        <TextButton variant="text">Bee a Swarm Saviour</TextButton>
+        <NavButton aria-label="Language" edge="end">
+          <LanguageIcon />
+        </NavButton>
+        <NavButton
+          id="profile-button"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "76px",
+          }}
+          aria-label="Language"
+          edge="end"
+          aria-controls="profile-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <MenuIcon />
+          <AccountCircleIcon />
+        </NavButton>
+        <Menu
+          id="profile-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "pofile-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,32 @@
 import React from "react";
+import Header from "../components/header";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import { Box } from "@mui/system";
+import { Slide } from "@mui/material";
+
+function HideOnScroll(props) {
+  const { children } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Box>
+      <HideOnScroll>
+        <Header />
+      </HideOnScroll>
+      <h1>Home Content</h1>
+      <Box sx={{ height: "200vh" }}></Box>
+    </Box>
   );
 };
 
